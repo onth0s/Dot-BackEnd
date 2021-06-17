7283caae30a3430dbe03333950c05a6c422a104a
onth0s <metaagnosis@gmail.com>
2021-06-17 13:45:42 +0200

    new branch

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,71 +1,11 @@
 const bcrypt = require('bcrypt');
 
-const UserCredential = require('../models/UserCredential.js');
 const AesopFable = require('../models/AesopFable.js');
 
 const CMSUser = require('../models/CMSUser.js');
 const CMSLoginLog = require('../models/CMSLoginLog.js');
 
-const getEmails = async (req, res) => {
-	try {
-		const doc = await UserCredential.find();
-
-		const arr = [];
-		doc.forEach(val => {
-			arr.push(val.email)
-		});
-
-		res.json(arr);
-	} catch (err) {
-		console.log('Error getting users emails:');
-		console.log(err);
-	}
-}
-
-const login = (req, res) => {
-	const credentials = req.body;
-
-	console.log('Credentials received:');
-	console.log(credentials);
-}
-
-const register = async (req, res) => {
-	const { email, password } = req.body;
-
-	console.log('\nCredentials received!');
-
-	try {
-		const doc = await UserCredential.findOne({ email });
-		if (!doc) {
-			const newUser = new UserCredential();
-			newUser.email = email;
-			newUser.password = await bcrypt.hash(password, 10);
-
-			try {
-				const doc = await newUser.save();
-				console.log(`UserCredential '${email}' registered successfully!`);
-				console.log(doc);
-			} catch (err) {
-				console.log('Error saving UserCredential:');
-				console.log(err.message);
-			}
-
-			res.send(`UserCredential '${email}' registered successfully!`);
-			res.status(200).end();
-		} else {
-			console.log(`Email '${email}' is already taken!`);
-			return res.sendStatus(409);
-		}
-	} catch (err) {
-		console.log('Error finding UserCredential:');
-		console.log(err);
-	}
-}
-
-let counter = 0;
 const getFables = async (req, res) => {
-	console.log(`\ngetting Aesop fables... (${counter++})`);
-
 	try {
 		AesopFable.countDocuments().exec(async (err, count) => {
 			if (err) return res.sendStatus(404);
@@ -82,7 +22,7 @@ const getFables = async (req, res) => {
 			}
 		});
 	} catch (err) {
-		console.log(`Error counting a AesopFable model:`);
+		console.log(`Error counting the AesopFable model:`);
 		console.log(err);
 	}
 
@@ -137,17 +77,6 @@ const CMSLogin = async (req, res) => {
 			console.log('Error saving logging log:');
 			console.log(err);
 		}
-		// loginLog.save().then(doc => {
-		// 	console.log('Logging log saved successfully:');
-		// 	console.log(doc);
-
-		// 	return res.sendStatus(202);
-		// }).catch(err => {
-		// 	console.log('Error saving logging log:');
-		// 	console.log(err);
-
-		// 	return res.sendStatus(500);
-		// });
 	} catch (err) {
 		console.log('Error finding user:');
 		console.log(err);
